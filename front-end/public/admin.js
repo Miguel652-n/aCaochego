eee1cc20cb96aa8ebc318425f1de554a085d86af
Miguel652-n <mj15012008@gmail.com>
2026-06-23 15:44:33 -0300

    deixei de ser burro

--- front-end/public/admin.js
+++ front-end/public/admin.js
@@ -1,4 +1,4 @@
-const API_URL = "https://acaochego-production.up.railway.app";
+const API_URL = "https://back-end-production-f743.up.railway.app"; // URL da API
 
   // credenciais simples (altere aqui)
   const USUARIO = "admin";
--- front-end/public/form.js
+++ front-end/public/form.js
@@ -1,4 +1,4 @@
-const API_URL = "https://acaochego-production.up.railway.app";
+const API_URL = "https://back-end-production-f743.up.railway.app"; // URL da API
 
 const params = new URLSearchParams(window.location.search);
 const animalNome = params.get("animal") || "";
