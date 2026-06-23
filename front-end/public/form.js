30381acfe9572c31aae6044cc850faa5c7b3121e
Miguel652-n <mj15012008@gmail.com>
2026-06-23 15:54:13 -0300

    teste de rota

--- front-end/public/form.js
+++ front-end/public/form.js
@@ -1,5 +1,5 @@
-const API_URL = "https://back-end-production-f743.up.railway.app"; // URL da API
-
+const API_URL = ""; // URL da API
+//https://back-end-production-f743.up.railway.app
 const params = new URLSearchParams(window.location.search);
 const animalNome = params.get("animal") || "";
 
